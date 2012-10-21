c5e4911149e2c901b89567a437ad91804b7068ea
Garrett Vanhoy <gvanhoy@email.arizona.edu>
2012-10-21 07:50:28 -0700

    Adjusted versions.

--- assets/js/init.js
+++ assets/js/init.js
@@ -20,17 +20,6 @@
 					template: "{tweet_text}"
 				});
 				//Loading Gallery Page
-				$('a.song').click(function(){
-					var songname = $(this).attr('id');
-					$.mobile.changePage( "#song");
-					$(document).ready(function(){
-						$.get("http://127.0.0.1/Providence-Mobile/assets/songs/" + songname, function(data){
-							var firstline = data.indexOf("\n");
-							$('#songTitle').html(data.slice(0, firstline));
-							$('div.song').html(data.slice(firstline));
-						});
-					});
-				});
 				$('div.gallery-page')
 					.live('pageshow', function(e){
 						var 
